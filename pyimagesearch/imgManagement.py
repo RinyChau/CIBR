bb23ee2ca619eefd65a433ece2143f3d36067a14
Ring <226214208@qq.com>
2016-06-08 17:34:59 +0800

    add upload img function

--- pyimagesearch/imgManagement.py
+++ pyimagesearch/imgManagement.py
@@ -11,8 +11,10 @@ class ImgManagement:
     def saveFile(img_dir, image_file):
         file_name = secure_filename(image_file.filename)
         year = str(datetime.datetime.now().year)
+        month = str(datetime.datetime.now().month)
+        day = str(datetime.datetime.now().day)
         hour = str(datetime.datetime.now().hour)
-        dir = os.path.join(img_dir, year, hour)
+        dir = os.path.join(img_dir, year, month, day, hour)
         if not os.path.exists(dir):
             os.makedirs(dir)
         path = os.path.join(dir, file_name)
