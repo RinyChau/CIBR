c57bfb714f051ecc7967918035bbae72cdd38fdf
Ring <226214208@qq.com>
2016-06-14 16:42:22 +0800

    add upload img function

--- helper/UpdateImgPath.py
+++ helper/UpdateImgPath.py
@@ -6,9 +6,9 @@ db = client.CIBR
 collection = db.ImageFeature
 imgList = list(collection.find())
 for img in imgList:
-    if "ImageUrl" in imgList:
+    if "ImageUrl" in img:
         del img["Path"]
     else:
         img["Path"] = img["Path"].replace('app', 'static')
         print(img)
-    collection.update(img)
+    collection.replace_one(img)
