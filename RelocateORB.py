84d81689bd3a1abcf1474526b12926cd1216b68a
Ring <226214208@qq.com>
2016-11-06 17:43:44 +0800

    show location + detection

---
+++ RelocateORB.py
@@ -0,0 +1,30 @@
+from pymongo import MongoClient
+import time
+from pyimagesearch.RCNNClassifer import RCNNClassifier
+
+# initialize mongodb client
+client = MongoClient()
+
+# Content-based image retrieval database
+db = client.CIBR
+
+collection = db.ImageFeature
+newCollection = db.ORBFeature
+
+imgList = collection.find()
+count = 0
+start_time = time.time()
+
+for imgItem in imgList:
+    newItem = {'_id': imgItem["_id"], 'md5': imgItem['md5'], 'ORB': imgItem['md5']}
+    newCollection.insert_one(newItem)
+    del imgItem['ORB']
+    collection.replace_one({"_id": imgItem["_id"]}, imgItem)
+
+    count += 1
+    if count % 50 == 0:
+        print(count)
+        print(" --- %s seconds ---" % (time.time() - start_time))
+
+print(count)
+print("finish --- %s seconds ---" % (time.time() - start_time))
